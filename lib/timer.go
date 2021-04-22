c9a9d699ebca46bd2bc8908c0a2d71a06972736f
wdlyt <719896508@qq.com>
2021-04-23 01:00:45 +0800

    'timer'

--- lib/timer.go
+++ lib/timer.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"github.com/liu-jiangyuan/go_websocket/controller"
 	"log"
 	"runtime"
 	"sync"
@@ -11,11 +12,12 @@ type timer struct {
 	id int64
 	mutex sync.Mutex
 	stopChan chan int64
-	timerMap map[int64]*time.Timer
+	timerMap map[int64]TimerStruct
 }
 
 type TimerStruct struct {
 	D time.Duration
+	t *time.Timer
 	Args map[string]interface{}
 	Action func(args map[string]interface{}) map[string]interface{}
 }
@@ -27,7 +29,7 @@ func init() {
 		id:       0,
 		mutex:    sync.Mutex{},
 		stopChan: make(chan int64),
-		timerMap: make(map[int64]*time.Timer),
+		timerMap: make(map[int64]TimerStruct),
 	}
 	go func() {
 		for {
@@ -50,8 +52,10 @@ func (t *timer) newTimerId() int64 {
 
 //清除一个定时器
 func (t *timer) Clear(timerId int64) {
-	t.timerMap[timerId].Stop()
-	delete(t.timerMap,timerId)
+	if _, ok := t.timerMap[timerId];ok {
+		t.timerMap[timerId].t.Stop()
+		delete(t.timerMap,timerId)
+	}
 }
 
 //停止全部定时器
@@ -64,67 +68,51 @@ func (t *timer) ClearAll() {
 }
 
 //必须执行一次的定时器，无法销毁,带参数与不带参数两种
-func (t *timer) MustOnceAfterWithFunArgs(s TimerStruct) {
-	go time.AfterFunc(s.D, func() {
-		s.Action(s.Args)
-		//res := s.Action(s.Args)
-		//log.Printf("MustOnceAfterWithFunArgs:%+v",res)
+func (t *timer) MustOnceAfter (s TimerStruct) {
+	time.AfterFunc(s.D, func() {
+		//s.Action(s.Args)
+		res := s.Action(s.Args)
+		log.Printf("MustOnceAfter:%+v",res)
 	})
-}
-func (t *timer) MustOnceAfter(second time.Duration,action func()) {
-	go time.AfterFunc(second, action)
 }
 
 //一次性定时器,可根据timerId提前销毁，带参数与不带参数两种
-func (t *timer) AfterWithFuncArgs (s TimerStruct) int64 {
+
+func (t *timer) After (s TimerStruct) int64 {
 	timerId := t.newTimerId()
-	go time.AfterFunc(s.D, func() {
-		s.Action(s.Args)
-		//res := s.Action(s.Args)
-		//log.Printf("AfterWithFuncArgs:%+v",res)
-	})
-	return timerId
-}
-func (t *timer) After (second time.Duration,action func()) int64 {
-	timerId := t.newTimerId()
-	t.timerMap[timerId] = time.AfterFunc(second, action)
+	t.timerMap[timerId] = TimerStruct{
+		D:      s.D,
+		t:      time.AfterFunc(s.D, func() {
+			//s.Action(s.Args)
+			res := s.Action(s.Args)
+			log.Printf("After:%+v",res)}),
+		Args:   s.Args,
+		Action: s.Action,
+	}
 	return timerId
 }
 
 //周期定时器,带参数与不带参数两种
-func (t *timer) LoopWithFuncArgs (s TimerStruct,stop ...int) int64 {
+func (t *timer) Loop (s TimerStruct,stop ...int) int64 {
 	timerId := t.newTimerId()
-	go func(id int64,s TimerStruct) {
-		restTimes := 0
-		t.timerMap[id] = time.NewTimer(s.D)
-		for {
-			select{
-			case <- t.timerMap[id].C:
-				//循环终止，方便调试
-				if len(stop) > 1 && stop[0] == 1 && stop[1] <= restTimes {
-					t.Clear(id)
-					//终止当前协程
-					runtime.Goexit()
-				}
-				restTimes ++
-				t.timerMap[id].Reset(s.D)
-				s.Action(s.Args)
-				//res := s.Action(s.Args)
-				//log.Printf("LoopWithFuncArgs:%+v",res)
-			}
-		}
-	}(timerId,s)
-	return timerId
-}
-func (t *timer) Loop (second time.Duration,action func(),stop ...int) int64 {
-	timerId := t.newTimerId()
+	t.timerMap[timerId] = TimerStruct{
+		D:      s.D,
+		t:      time.NewTimer(s.D),
+		Args:   s.Args,
+		Action: s.Action,
+	}
 
-	go func(id int64) {
+	go func(id int64,s TimerStruct) {
+		defer func() {
+			if err := recover(); err != nil {
+				s.t.Stop()
+				t.Clear(id)
+			}
+		}()
 		restTimes := 0
-		t.timerMap[id] = time.NewTimer(second)
 		for {
 			select{
-			case <- t.timerMap[id].C:
+			case <- t.timerMap[id].t.C:
 				//循环终止，方便调试
 				if len(stop) > 1 && stop[0] == 1 && stop[1] <= restTimes {
 					t.Clear(id)
@@ -132,56 +120,46 @@ func (t *timer) Loop (second time.Duration,action func(),stop ...int) int64 {
 					runtime.Goexit()
 				}
 				restTimes ++
-				t.timerMap[id].Reset(second)
-				action()
+				t.timerMap[id].t.Reset(s.D)
+				//s.Action(s.Args)
+				res := s.Action(s.Args)
+				log.Printf("Loop:%+v",res)
 			}
 		}
-	}(timerId)
+	}(timerId,t.timerMap[timerId])
+
 	return timerId
 }
 
 func test() {
-	id := Timer.After(time.Second * 3, func() {
-		log.Printf("timer After func:%+v","1")
+	id := Timer.After(TimerStruct{
+		D:      7 * time.Second,
+		Args: map[string]interface{}{"method":"AfterWithFuncArgsNoClear"},
+		Action: controller.Index,
 	})
-	log.Printf("timer After id:%+v",id)
-	id2 := Timer.After(time.Second * 5, func() {
-		log.Printf("timer After func:%+v","id2")
+	log.Printf("Timer After id:%+v",id)
+	id2 := Timer.After(TimerStruct{
+		D:      9 * time.Second,
+		Args: map[string]interface{}{"method":"AfterWithFuncArgs"},
+		Action: controller.Index,
 	})
-	log.Printf("timer Tick id:%+v",id2)
+	log.Printf("Timer Tick id:%+v",id2)
 
-	id3 := Timer.Loop (time.Second * 3, func() {
-		log.Printf("timer After func:%+v","id3")
-	})
-	log.Printf("timer Tick id:%+v",id3)
-	Timer.MustOnceAfter(time.Second * 15 , func() {
-		log.Printf("timer MustOnceAfter func:%+v","id4")
-	})
-	Timer.Clear(id)
+	id3 := Timer.Loop (TimerStruct{
+		D:      1 * time.Second,
+		Args: map[string]interface{}{"method":"LoopWithFuncArgs"},
+		Action: controller.Index,
+	},1,5)
+	log.Printf("Timer Tick id:%+v",id3)
 
-	//id := Timer.AfterWithFuncArgs(TimerStruct{
-	//	D:      7 * time.Second,
-	//	Args: map[string]interface{}{"method":"AfterWithFuncArgsNoClear"},
-	//	Action: controller.Index,
-	//})
-	//log.Printf("timer After id:%+v",id)
-	//id2 := Timer.AfterWithFuncArgs(TimerStruct{
-	//	D:      1 * time.Second,
-	//	Args: map[string]interface{}{"method":"AfterWithFuncArgs"},
-	//	Action: controller.Index,
-	//})
-	//log.Printf("timer Tick id:%+v",id2)
-	//
-	//id3 := Timer.LoopWithFuncArgs (TimerStruct{
-	//	D:      3 * time.Second,
-	//	Args: map[string]interface{}{"method":"LoopWithFuncArgs"},
-	//	Action: controller.Index,
-	//})
-	//Timer.MustOnceAfterWithFunArgs(TimerStruct{
-	//	D:      3 * time.Second,
-	//	Args: map[string]interface{}{"method":"MustOnceAfterWithFunArgs"},
-	//	Action: controller.Index,
-	//})
-	//log.Printf("timer Tick id:%+v",id3)
-	//Timer.Clear(id)
+	Timer.MustOnceAfter(TimerStruct{
+		D:      15 * time.Second,
+		Args: map[string]interface{}{"method":"MustOnceAfterWithFunArgs"},
+		Action: controller.Index,
+	})
+
+	time.AfterFunc(3 * time.Second, func() {
+		Timer.Clear(id3)
+	})
+	Timer.Clear(id3)
 }
--- socket.go
+++ socket.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/liu-jiangyuan/go_websocket/conf"
+	"github.com/liu-jiangyuan/go_websocket/controller"
 	"github.com/liu-jiangyuan/go_websocket/engine"
+	"github.com/liu-jiangyuan/go_websocket/lib"
 	"log"
+	"time"
 )
 
 func main()  {
@@ -12,6 +15,36 @@ func main()  {
 	conf.InitRoute()
 	e.SetHost("0.0.0.0")
 	e.SetPort("8089")
+	id := lib.Timer.After(lib.TimerStruct{
+		D:      7 * time.Second,
+		Args: map[string]interface{}{"method":"AfterWithFuncArgsNoClear"},
+		Action: controller.Index,
+	})
+	log.Printf("lib.Timer After id:%+v",id)
+	id2 := lib.Timer.After(lib.TimerStruct{
+		D:      9 * time.Second,
+		Args: map[string]interface{}{"method":"AfterWithFuncArgs"},
+		Action: controller.Index,
+	})
+	log.Printf("lib.Timer Tick id:%+v",id2)
+
+	id3 := lib.Timer.Loop (lib.TimerStruct{
+		D:      1 * time.Second,
+		Args: map[string]interface{}{"method":"LoopWithFuncArgs"},
+		Action: controller.Index,
+	},1,5)
+	log.Printf("lib.Timer Tick id:%+v",id3)
+
+	lib.Timer.MustOnceAfter(lib.TimerStruct{
+		D:      15 * time.Second,
+		Args: map[string]interface{}{"method":"MustOnceAfterWithFunArgs"},
+		Action: controller.Index,
+	})
+
+	time.AfterFunc(3 * time.Second, func() {
+		lib.Timer.Clear(id3)
+	})
+	lib.Timer.Clear(id3)
 
 	//路由设置 可在此处设置，也可以在conf/router.go配置文件设置
 	//e.AddRoute("send",controller.Send)
